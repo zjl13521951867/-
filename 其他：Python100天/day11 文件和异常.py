0210fb972b58740e5aa3a0f6f4fb7d8a47571b74
a-zhangjiale <13521951867@163.com>
2021-08-10 18:28:16 +0800

    Python100 进程和线程

--- 其他：Python100天/day11 文件和异常.py
+++ 其他：Python100天/day11 文件和异常.py
@@ -1,3 +1,16 @@
+'''
+操作符的基本用法：
+    r    以只读方式打开文件。这是默认模式。文件必须存在，不存在抛出错误
+    rb    以二进制格式打开一个文件用于只读。
+    r+    打开一个文件用于读写。文件指针将会放在文件的开头。读完就追加。
+    w    打开一个文件只用于写入。如果该文件已存在则将其覆盖。如果该文件不存在，创建新文件。
+    w+    打开一个文件用于读写。如果该文件已存在则将其覆盖。如果该文件不存在，创建新文件。
+    a    打开一个文件用于追加。如果该文件已存在，文件指针将会放在文件的结尾。也就是说，新的内容将会被写入到已有内容之后。如果该文件不存在，创建新文件进行写入。
+    a+    打开一个文件用于读写。如果该文件已存在，文件指针将会放在文件的结尾。文件打开时会是追加模式。如果该文件不存在，创建新文件用于读写。
+    注：后面有带b的方式，不需要考虑编码方式。有带+号的，则可读可写，不过它们之间还是有区别的
+'''
+
+
 # 文件操作使用open()函数
 # open(src,mode,encoding) 路径 读/写... 编码
 # with open(src,mode,encoding) as f  等价于  f = open(src,mode,encoding)
@@ -6,6 +19,7 @@
 # FileNotFoundError 文件找不到
 # LookupError 指定了未知的编码会引发
 # UnicodeDecodeError 而如果读取文件时无法按指定方式解码
+# IOError 文件打开失败(通用类型，所有操作异常都可以通过这个抛出)
 
 # file1 = open(r'C:\Users\13521\Desktop\Python\-\pythonModule\file1.txt',mode='r',encoding='utf-8')
 # print(file1)
@@ -59,26 +73,30 @@
 
 
 # 练习，将99乘法表写入一个新文件内
-# def multiplication_table():
-#     try:
-#         with open(r'C:\Users\13521\Desktop\Python\-\pythonModule\file2.txt',mode='a',encoding='utf-8') as file2:
-#             i = 0
-#             while i < 9:
-#                 i = i + 1
-#                 j = 0 
-#                 a = ''
-#                 while j < i:
-#                     space = '\t'
-#                     j = j + 1
-#                     a = f'{j}*{i}={i*j}'
-#                     if j == i:
-#                         space = '\n'
-#                     file2.write(a + space)
-#     except IOError:
-#         print('文件写入时发生错误，请稍后重试')
-#     finally:
-#         file2 and file2.close()
-# multiplication_table()
+# 方法1：内存循环内每次写入
+# 方法2：保存在一个字符串内，在循环外写入
+def multiplication_table():
+    file_text = ''
+    try:
+        with open(r'C:\Users\13521\Desktop\Python\-\pythonModule\file2.txt',mode='a',encoding='utf-8') as file2:
+            i = 0
+            while i < 9:
+                i = i + 1
+                j = 0 
+                a = ''
+                while j < i:
+                    space = '\t'
+                    j = j + 1
+                    a = f'{j}*{i}={i*j}'
+                    if j == i:
+                        space = '\n'
+                    file_text = file_text + a + space 
+            file2.write(file_text)
+    except IOError:
+        print('文件写入时发生错误，请稍后重试')
+    finally:
+        file2 and file2.close()
+multiplication_table()
 
 
 # 二进制文件的读写  而进入模式为b
@@ -140,44 +158,44 @@
 import os 
 # 当前目录 (根目录路径)
 now_path = os.getcwd()
-print('当前路径',now_path)
+# print('当前路径',now_path)
 
 # 当前路径下的所有文件
 # os.listdir(文件夹路径)
 list = os.listdir(os.getcwd())
-print('当前路径下的所有文件',list)
+# print('当前路径下的所有文件',list)
 for x in list:
     pass 
     # print(f'{now_path}\\{x}')
 
 # 绝对路径  .表示当前    ../表示上一级     ./视频学习 表示下级目录
-print('绝对',os.path.abspath('./视频学习'))
+# print('绝对',os.path.abspath('./视频学习'))
 
 # 查看路径的 "文件夹和文件名部分" 元组类型
 # 例：c:\Users\13521\Desktop\Python\- (这里是文件夹)    7：面向对象高级编程 (这里是文件名)
 # 返回一个元组，第一位是文件夹名称   第二位是文件名称
-print('文件夹和文件名',os.path.split(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
-print('文件夹和文件名',os.path.split(r'c:\Users\13521\Desktop\Python\-\.git'))
+# print('文件夹和文件名',os.path.split(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
+# print('文件夹和文件名',os.path.split(r'c:\Users\13521\Desktop\Python\-\.git'))
 
 # 获取路径的 "文件夹部分"
-print('路径的文件夹部分',os.path.dirname(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
+# print('路径的文件夹部分',os.path.dirname(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
 
 # 获取路径的 "文件名"
-print('路径的文件名称',os.path.basename(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
+# print('路径的文件名称',os.path.basename(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
 
 
 # 文件时间
 # 最后修改时间
-print(os.path.getmtime(r'C:\Users\13521\Desktop\Python\-\pythonModule\file4.json'))
+# print(os.path.getmtime(r'C:\Users\13521\Desktop\Python\-\pythonModule\file4.json'))
 # 最后的访问时间
-print(os.path.getatime(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
+# print(os.path.getatime(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
 # 创建时间
-print(os.path.getctime(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
+# print(os.path.getctime(r'c:\Users\13521\Desktop\Python\-\7：面向对象高级编程'))
 
 
 # 查看文件大小，文件夹会返回0
 # 单位：byte(字节)
-print('文件大小',os.path.getsize(r'C:\Users\13521\Desktop\Python\-\pythonModule\file3.json'))
+# print('文件大小',os.path.getsize(r'C:\Users\13521\Desktop\Python\-\pythonModule\file3.json'))
 
 
 # 练习 查找目录下的最新文件
@@ -185,9 +203,42 @@ print('文件大小',os.path.getsize(r'C:\Users\13521\Desktop\Python\-\pythonMod
 list = os.listdir(os.getcwd() + '\其他：Python100天')
 # print(list)
 list = sorted(list, key=lambda x: os.path.getmtime(f'{os.getcwd()}\其他：Python100天\{x}'), reverse=True)
-print('列表',list)
+# print('列表',list)
 for x in list:
-    path = f'{os.getcwd()}\其他：Python100天\{x}'
+    pass 
+    # path = f'{os.getcwd()}\其他：Python100天\{x}'
     # print(path)
     # print(os.getcwd() + '\\' + x)
-    print(os.path.getmtime(path))
+    # print(os.path.getmtime(path))
+
+
+# 文件操作失败的异常 IOError
+test_path = os.getcwd() + '\其他：Python100天\day1 图形用户界面和游戏开发.py'
+test_file = ''
+try:
+    test_file = open(test_path,mode='r',encoding='utf-8')
+    # print(test_file)
+    # print(test_file.read())
+except IOError:
+    print('文件打开失败')
+finally:
+    test_file and test_file.close()
+
+
+# 接口请求JSON数据
+# 请求返回的数据需要通过 .text 或 .content转换成正常阅读的 json 或者 html
+import requests
+import json
+def get_news():
+    url = 'http://api.tianapi.com/esports/index?key=094ca973589465b4558f9d1ab0275ed0&num=50'  
+    result = requests.get(url)
+    # print('转码前',result) # <Response [200]>
+    # result = result.text # 转完正常json
+    result = result.content # 转完\xe5\x8d ，还需要 result.decode(utf-8)
+    print('转码后',result)
+    print('解码后',result.decode('utf-8'))
+    result = json.loads(result)
+    new_list = result['newslist']
+    for n in new_list:
+        print(n['title'])
+# get_news()
---
+++ 其他：Python100天/day13 进程和线程.py
@@ -0,0 +1,185 @@
+'''
+多线程的优势：
+    1：多个线程同时进行，减少了功能(代码)的运行时间
+    2：锁的用处是多个人操作同一个状态 例：多个人取钱，但是银行的总额是固定的，所以需要加锁逐个处理
+'''
+
+
+
+
+
+from random import randint
+from time import time, sleep
+
+# 默认模式，每次下载完才能下载另一个
+# def donload_task(file_name):
+#     print(f'开始下载 {file_name}...')
+#     sleep_time = randint(5,10)
+#     sleep(sleep_time)
+#     print(f'下载 {file_name} 花费了 {sleep_time}秒')
+
+# def main():
+#     star = time()
+#     donload_task('功夫.mkv')
+#     donload_task('蜡笔小新.gif')
+#     end = time()
+#     print('一共花费了 %d 秒'%(end-star))
+# main()
+
+
+
+'''
+1：多进程的优点就是，多个任务同时进行，减少程序的等待时间
+2：多进程的代码需要执行在  if __name__ == 'main'内
+3：Process(target=fn, args=(参数元组))，返回一个进程对象
+    进程对象.star()开始执行此进程
+    进程对象.join()，join后的代码会等到当前进程 '执行完成' 再执行
+'''
+# from multiprocessing import Process
+# from os import getpid, startfile
+# from random import randint
+# from time import time, sleep
+
+# def download_task(file_name):
+#     print(f'正在下载 {file_name}...')
+#     print(f'当前进程号 [{getpid()}]')
+#     donload_time = randint(5,10)
+#     sleep(donload_time)
+#     print(f'下载 {file_name} 用了 {donload_time}秒')
+# def main():
+#     start = time()
+#     p1 = Process(target=download_task, args=('Python从入门到住院.pdf', ))
+#     p1.start()
+#     p2 = Process(target=download_task, args=('Peking Hot.avi', ))
+#     p2.start()
+#     p1.join()
+#     p2.join()
+#     end = time()
+#     print('总共耗费了%d秒.' % (end - start))
+# if __name__ == '__main__':
+#     main()
+
+
+
+'''
+1：线程
+2：Thread(target=fn, args=(元组参数,)) 返回线程对象
+    线程对象.start() 开始执行此进程
+    线程对象.join() 等线程的代码执行完成才执行join()方法下的代码
+3: 线程的run()方法，线程在生成后会自动执行run()函数，所以写子类的时候会有个run()函数
+'''
+# from random import randint
+# from threading import Thread
+# from time import time, sleep
+
+# def download_task(file_name):
+#     print(f'正在下载 {file_name}...')
+#     donload_time = randint(5,10)
+#     sleep(donload_time)
+#     print(f'下载 {file_name} 用了 {donload_time}秒')
+# def main():
+#     start = time()
+#     p1 = Thread(target=download_task, args=('Python从入门到住院.pdf', ))
+#     p1.start()
+#     p2 = Thread(target=download_task, args=('Peking Hot.avi', ))
+#     p2.start()
+#     p1.join()
+#     p2.join()
+#     end = time()
+#     print('总共耗费了%d秒.' % (end - start))
+# if __name__ == '__main__':
+#     main()
+
+
+'''
+原版100个账号向银行内存钱：
+    线程的代码时同时进行的，所有线程拿到的初始化余额都是0，每个账户都时往0上 +1，最终得到的就只能是1
+'''
+# from time import sleep
+# from threading import Thread,Lock 
+
+# class Account(object):
+#     # 初始化余额为0
+#     def __init__(self):
+#         self._blance = 0
+#         self._lock = Lock()
+
+#     # 计算金额
+#     def deposit(self,money):
+#         # 先获取锁才能执行后续的代码
+#         #self._lock.acquire()
+#         # try:
+#         # 当前金额
+#         new_blance = self._blance + money 
+#         # 模拟银行的存款时间
+#         sleep(0.0001)
+#         # 存款过后重置余额
+#         self._blance = new_blance
+
+#         print('当前',self._blance)
+#         #self._lock.release()
+#         # finally:
+#         #     # 释放锁
+#         #     self._lock.release()
+#     # 获取存款余额
+#     @property
+#     def blance(self):
+#         return self._blance
+
+# class AddMoney(Thread):
+#     def __init__(self,account,money):
+#         super().__init__()
+#         self._account = account
+#         self._money = money
+    
+#     def run(self):
+#         self._account.deposit(self._money)
+
+# def main():
+#     account = Account()
+#     threads = []
+
+#     for _ in range(100):
+#         t = AddMoney(account,1)
+#         threads.append(t)
+#         t.start()
+        
+#     for t in threads:
+#         t.join()
+#     print('账户余额',account.blance,'元')
+
+
+
+'''
+两个人同时从银行取钱，银行一共又1000元，大事难事两人分别取800
+
+'''
+from time import sleep
+from threading import Thread,Lock 
+count_money = 1000
+def get_money(name,money):
+    global count_money
+    lock = Lock()
+    lock.acquire()
+    print(f'{name} 正在取钱')
+    sleep(1)
+    if count_money > money:
+        count_money = count_money - money 
+    else:
+        print('余额不足')
+    lock.release()
+   
+
+def main():
+    t1 = Thread(target=get_money, args=('张三',800)) 
+    t1.start()
+    t2 = Thread(target=get_money, args=('王麻子',800,)) 
+    t2.start()
+    
+    t1.join()
+    t2.join()
+    print('余额还有',count_money)
+
+if __name__ == '__main__':
+    main()
+
